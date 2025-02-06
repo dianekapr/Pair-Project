dd9ae7438136864edcf7e0bcc5ba0036c4477311
Dian <74811382+dianekapr@users.noreply.github.com>
2025-02-06 19:33:45 +0700

    Update seller.js

--- routers/seller.js
+++ routers/seller.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const Controller = require('../controllers/custC');
+const Controller = require('../controllers/controller');
+const Seller = require('../controllers/sellerC');
 const seller = express.Router();
 
 seller.get('/register')
 seller.post('/register')
 seller.get('/login')
 seller.post('/login')
+seller.get('/logout')
 seller.get('/profile')
 seller.get('/profile/setting')
 seller.post('/profile/setting')
@@ -17,5 +19,22 @@ seller.post('/addProduct')
 seller.get('/products/:ProductId/edit')
 seller.get('/products/:ProductId/edit')
 seller.get('/categories')
+
+seller.get('/register',Seller.getRegister)
+seller.post('/register', Seller.postRegister)
+
+seller.get('/login', Seller.getSellersLogin)
+seller.post('/login', Seller.postSellersLogin)
+
+seller.get('/',Seller.showListProduct)
+seller.get('/products/:id', Seller.getDetailProduct)
+
+seller.get('/addProduct',Seller.addProduct)
+seller.post('/addProduct', Seller.postProduct)
+
+seller.get('/products/:id/edit', Seller.getProductEdit)
+seller.post('/products/:id/edit', Seller.postProductEdit)
+
+seller.get('/delete')
 
 module.exports = seller;
